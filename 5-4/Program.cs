9fe227039c93a236b9d51204a577e61719688ab8
MarthaLRigby <martha.l.rigby@gmail.com>
2015-06-03 00:39:16 +0100

    Ex 5-4

---
+++ 5-4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0, j = 25; ; i++, j--)
+            {
+                if (i > j)
+                {
+                    Console.WriteLine("Crossed over!");
+                    break;
+                }
+                Console.WriteLine(i + " " + j);
+            }
+        }
+    }
+}
